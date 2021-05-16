017d88f9087c85da0d9c5b634185684274624873
venukarri <venu_kvks@yahoo.com>
2021-05-16 23:31:42 +0530

    Add comments

--- src/com/oportun/atm/ATMServiceImpl.java
+++ src/com/oportun/atm/ATMServiceImpl.java
@@ -17,6 +17,10 @@ public class ATMServiceImpl {
         calculateBalance();
     }
 
+    /**
+     * Method to initialize ATM cash Tray with required
+     * denominations and number of bills for each denomination.
+     */
     private void loadDefaultTray() {
         dollarBillsTray.put(20, 0);
         dollarBillsTray.put(10, 0);
@@ -28,33 +32,44 @@ public class ATMServiceImpl {
         // dollarBillsTray.put(50, 0);
     }
 
+    /**
+     * Method to calculate balance based on available denominations and number od bills.
+     */
     private void calculateBalance() {
         dollarBillsTray.forEach((denomination, noOfBills) -> {
             creditAmount(Long.valueOf(denomination * noOfBills));
         });
     }
 
+    /**
+     * Service to deposit.
+     */
     public void deposit() {
         System.out.println("<-- Deposit Screen: Enter Denominations you have as prompted -->");
 
         this.billsDepositedStats.clear();
         boolean isAllNoOfBillsZero = true;
 
+        // Prompt user to enter number of bills for each denomination.
         for(Integer denomination : dollarBillsTray.keySet()) {
             System.out.print("Enter number of "+denomination+"s:");
             int noOfBillsEntered = scanner.nextInt();
+            // validate if negative number of bills to deposit is entered.
             if(isNoOfBillsDepositedNegative(noOfBillsEntered))
                 return;
+            // If number of bills entered grater than zero then change the flag.
             isAllNoOfBillsZero = isNoOfBillsDepositedZero(noOfBillsEntered);
             this.billsDepositedStats.put(denomination, noOfBillsEntered);
         }
 
+        // Validate if all number of bills entered are zero.
         if(validateAllNoOfBillsForZero(isAllNoOfBillsZero))
             return;
 
         System.out.print("Are you sure want to deposit? Select 1. Yes 2. No :");
         int depositConfirmation = scanner.nextInt();
         if(depositConfirmation == 1) {
+            // Iterate and add to bills count and total balance.
             this.billsDepositedStats.forEach((depositedDenomination, noOfBillsDeposited) -> {
                 this.dollarBillsTray.put(
                         depositedDenomination,
@@ -67,11 +82,15 @@ public class ATMServiceImpl {
         }
     }
 
+    /**
+     * Service to withdraw.
+     */
     public void withdraw() {
         System.out.println("<-- Withdraw Screen -->");
         System.out.print("Enter the amount you would like to withdraw:");
         int withdrawAmt = scanner.nextInt();
 
+        // Check for zero, negative and over balance.
         if(isWithdrawAmtEnteredInvalid(withdrawAmt))
             return;
 
@@ -87,11 +106,13 @@ public class ATMServiceImpl {
 
                  if(noOfBillsAvailable > 0) {
                      tempWithdrawAmt = dispenseDenomination(denomination, noOfBillsAvailable, tempWithdrawAmt, billsWithdrawStats);
+                     // If withdraw amount is evaluated then exit no need to iterate.
                      if(tempWithdrawAmt <= 0) {
                           break;
                      }
                  }
              }
+             // Check if bills are insufficient to dispense.
              if(unableToDispenseBills(tempWithdrawAmt))
                  return;
 
@@ -102,6 +123,11 @@ public class ATMServiceImpl {
         }
     }
 
+    /**
+     * Method to check if bills are insufficient to dispense.
+     * @param tempWithdrawAmt
+     * @return
+     */
     private boolean unableToDispenseBills(int tempWithdrawAmt) {
         if(tempWithdrawAmt > 0) {
             System.out.println("Sorry, unable to dispense bills. Please try different amount");
@@ -110,6 +136,11 @@ public class ATMServiceImpl {
             return false;
         }
     }
+
+    /**
+     * Method to evaluate bills to be dispensed, decrease number of bills, debut from balance.
+     * @param billsWithdrawStats
+     */
     private void evaluateBillsTobeDispensed(Map<Integer,Integer> billsWithdrawStats) {
         StringBuffer dispenseSummaryMsg = new StringBuffer();
         billsWithdrawStats.forEach((dispensedDenomination, noOfBillsDispensed) -> {
@@ -124,10 +155,20 @@ public class ATMServiceImpl {
         System.out.println("Dispensed: "+dispenseMsg.substring(0, (dispenseMsg.length() - 2)));
     }
 
+    /**
+     * Method to evaluate number of bills can be dispensed based on the withdraw amount.
+     *
+     * @param denomination
+     * @param noOfBillsAvailable
+     * @param amount
+     * @param billsWithdrawStats
+     * @return
+     */
     private int dispenseDenomination(Integer denomination, Integer noOfBillsAvailable,
                                      int amount, Map<Integer,Integer> billsWithdrawStats) {
         int billsCanBeDispensed = 0;
         for(int count=1; count <= noOfBillsAvailable; count++) {
+            // If denomination greater than temp amount then break. No need to iterate further.
             if(denomination <= amount) {
                 billsCanBeDispensed++;
                 amount = amount - denomination;
@@ -140,6 +181,12 @@ public class ATMServiceImpl {
        return amount;
     }
 
+    /**
+     * Method to validate withdraw amount for zero, negative and over balance.
+     *
+     * @param withdrawAmt
+     * @return
+     */
     private boolean isWithdrawAmtEnteredInvalid(int withdrawAmt) {
         if((withdrawAmt <= 0) || (withdrawAmt > this.totalBalance)) {
             System.out.println("Incorrect or insufficient funds.");
@@ -149,6 +196,12 @@ public class ATMServiceImpl {
         }
     }
 
+    /**
+     * Method to validate if number of bills to be deposited is negative.
+     *
+     * @param noOfBills
+     * @return
+     */
     private boolean isNoOfBillsDepositedNegative(Integer noOfBills) {
        if(noOfBills < 0) {
            System.out.println("Incorrect deposit amount.");
@@ -158,6 +211,11 @@ public class ATMServiceImpl {
        }
     }
 
+    /**
+     * Methos to validate if of bills to be deposited is zero.
+     * @param noOfBills
+     * @return
+     */
     private boolean isNoOfBillsDepositedZero(Integer noOfBills) {
        if(noOfBills == 0) {
            return true;
@@ -166,6 +224,12 @@ public class ATMServiceImpl {
        }
     }
 
+    /**
+     * Method to validate if deposited denomination number of bills are zero.
+     *
+     * @param isAllNoOfBillsZero
+     * @return
+     */
     private boolean validateAllNoOfBillsForZero(boolean isAllNoOfBillsZero) {
         if(isAllNoOfBillsZero) {
             System.out.println("Deposit amount cannot be zero.");
@@ -175,6 +239,9 @@ public class ATMServiceImpl {
         }
     }
 
+    /**
+     * Method to prompt user for next transaction.
+     */
     private void promptForNextTrans() {
         System.out.print("Would you like to perform another transaction? Select 1. Yes 2. No :");
         int nextTransInput = this.scanner.nextInt();
@@ -184,6 +251,9 @@ public class ATMServiceImpl {
         System.out.println("<===== Another Transaction =====>");
     }
 
+    /**
+     * Method to display available balance with denominations.
+     */
     private void displayBalanceWithDenominations() {
         StringBuffer depositSummaryMsg = new StringBuffer();
         dollarBillsTray.forEach((denomination, noOfBills) -> {
