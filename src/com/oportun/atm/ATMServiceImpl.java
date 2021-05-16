dbb945ae2e3a5ebd0b4cf73bd2b39e02d82eac0d
venukarri <venu_kvks@yahoo.com>
2021-05-16 22:58:29 +0530

    Add conditions to withdraw and deposit

--- src/com/oportun/atm/ATMServiceImpl.java
+++ src/com/oportun/atm/ATMServiceImpl.java
@@ -4,22 +4,28 @@ import java.util.*;
 
 public class ATMServiceImpl {
 
-    Comparator<Integer> denominationComparetor =
-            (denomination1, denomination2) -> (denomination2.compareTo(denomination1)) ;
+    Comparator<Integer> denominationComparetor = Comparator.reverseOrder();
     private Map<Integer, Integer> dollarBillsTray = new TreeMap<>(denominationComparetor);
     Map<Integer, Integer> billsDepositedStats = new TreeMap<>();
-    Map<Integer, Integer> billsWithdrawStats = new TreeMap<>();
+    Map<Integer, Integer> billsWithdrawStats = new TreeMap<>(denominationComparetor);
 
     private Long totalBalance = 0l;
     Scanner scanner = new Scanner(System.in);
 
     public ATMServiceImpl() {
+        loadDefaultTray();
+        calculateBalance();
+    }
+
+    private void loadDefaultTray() {
         dollarBillsTray.put(20, 0);
         dollarBillsTray.put(10, 0);
         dollarBillsTray.put(5, 0);
         dollarBillsTray.put(1, 0);
 
-        calculateBalance();
+        // For future support
+        // dollarBillsTray.put(100, 0);
+        // dollarBillsTray.put(50, 0);
     }
 
     private void calculateBalance() {
@@ -75,21 +81,62 @@ public class ATMServiceImpl {
              this.billsWithdrawStats.clear();
              int tempWithdrawAmt = withdrawAmt;
              for(Map.Entry<Integer, Integer> denominationEntry : this.dollarBillsTray.entrySet()) {
-                 int denomination = denominationEntry.getKey();
-                 int noOfBillsAvailable = denominationEntry.getValue();
-                 if(noOfBillsAvailable > 0) {
-                     tempWithdrawAmt = dispenseDenomination(denomination, tempWithdrawAmt);
-                     if(tempWithdrawAmt <= 0) {
 
+                 Integer denomination = denominationEntry.getKey();
+                 Integer noOfBillsAvailable = denominationEntry.getValue();
+
+                 if(noOfBillsAvailable > 0) {
+                     tempWithdrawAmt = dispenseDenomination(denomination, noOfBillsAvailable, tempWithdrawAmt, billsWithdrawStats);
+                     if(tempWithdrawAmt <= 0) {
+                          break;
                      }
                  }
              }
+             if(unableToDispenseBills(tempWithdrawAmt))
+                 return;
+
+            evaluateBillsTobeDispensed(billsWithdrawStats);
+            displayBalanceWithDenominations();
         } else {
             System.out.println("Withdraw cancelled.");
         }
     }
 
-    private int dispenseDenomination(int denomination, int amount) {
+    private boolean unableToDispenseBills(int tempWithdrawAmt) {
+        if(tempWithdrawAmt > 0) {
+            System.out.println("Sorry, unable to dispense bills. Please try different amount");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private void evaluateBillsTobeDispensed(Map<Integer,Integer> billsWithdrawStats) {
+        StringBuffer dispenseSummaryMsg = new StringBuffer();
+        billsWithdrawStats.forEach((dispensedDenomination, noOfBillsDispensed) -> {
+            this.dollarBillsTray.put(
+                    dispensedDenomination,
+                    (this.dollarBillsTray.get(dispensedDenomination) - noOfBillsDispensed));
+            debitAmount(Long.valueOf(dispensedDenomination * noOfBillsDispensed));
+            dispenseSummaryMsg.append(dispensedDenomination+"s="+noOfBillsDispensed+", ");
+        });
+        // debitAmount(Long.valueOf(withdrawAmt));
+        String dispenseMsg = dispenseSummaryMsg.toString();
+        System.out.println("Dispensed: "+dispenseMsg.substring(0, (dispenseMsg.length() - 2)));
+    }
+
+    private int dispenseDenomination(Integer denomination, Integer noOfBillsAvailable,
+                                     int amount, Map<Integer,Integer> billsWithdrawStats) {
+        int billsCanBeDispensed = 0;
+        for(int count=1; count <= noOfBillsAvailable; count++) {
+            if(denomination <= amount) {
+                billsCanBeDispensed++;
+                amount = amount - denomination;
+            } else {
+                break;
+            }
+        }
+        if(billsCanBeDispensed > 0)
+           billsWithdrawStats.put(denomination, billsCanBeDispensed);
        return amount;
     }
 
@@ -134,7 +181,7 @@ public class ATMServiceImpl {
         if(nextTransInput == 2) {
             closeSession();
         }
-        System.out.println("<--- Another Transaction --->");
+        System.out.println("<===== Another Transaction =====>");
     }
 
     private void displayBalanceWithDenominations() {
@@ -154,7 +201,8 @@ public class ATMServiceImpl {
             System.out.println("Available ATM Services");
             System.out.println("Select 1 to Withdraw");
             System.out.println("Select 2 to Deposit");
-            System.out.println("Select 3 to EXIT");
+            System.out.println("Select 3 to Display Balance");
+            System.out.println("Select 4 to Exit");
             System.out.print("Select the operation you want to perform:");
 
             int optionSelected = atmService.scanner.nextInt();
@@ -166,7 +214,10 @@ public class ATMServiceImpl {
                 case 2: atmService.deposit();
                         atmService.promptForNextTrans();
                         break;
-                case 3: atmService.closeSession();
+                case 3: atmService.displayBalanceWithDenominations();
+                        atmService.promptForNextTrans();
+                        break;
+                case 4: atmService.closeSession();
             }
         }
     }
@@ -184,7 +235,7 @@ public class ATMServiceImpl {
         this.totalBalance = this.totalBalance + amtToCredit;
     }
 
-    private void deditAmount(Long amtToDebit) {
+    private void debitAmount(Long amtToDebit) {
         this.totalBalance = this.totalBalance - amtToDebit;
     }
 }
